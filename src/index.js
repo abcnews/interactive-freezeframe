82366187bbc3dc18e3b6b97e056f5214fbd9fd50
Nathan Hoad <nathan@nathanhoad.net>
2019-10-25 11:33:59 +1000

    Commenting out the productionUnit check... for now

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import { loadFreezeframes } from './util';
 import TeamError from './components/TeamError';
 
 function init() {
-  // Detect a valid production unit
-  const productionUnit = document.querySelector('meta[name="ABC.productionUnit"]');
-  if (!productionUnit || ['Story Lab', 'EDL team'].indexOf(productionUnit.getAttribute('content')) === -1) {
-    // If we are in preview...
-    if (document.location.href.indexOf('nucwed') > -1) {
-      // ...mount a message about talking to Tim
-      const anchor = document.querySelector('a[name^="freezeframe"]');
-      const mountNode = document.createElement('div');
-      anchor.parentElement.insertBefore(mountNode, anchor);
-      render(<TeamError />, mountNode);
-    }
-    // Either way, stop
-    return;
-  }
+  // // Detect a valid production unit
+  // const productionUnit = document.querySelector('meta[name="ABC.productionUnit"]');
+  // if (!productionUnit || ['Story Lab', 'EDL team'].indexOf(productionUnit.getAttribute('content')) === -1) {
+  //   // If we are in preview...
+  //   if (document.location.href.indexOf('nucwed') > -1) {
+  //     // ...mount a message about talking to Tim
+  //     const anchor = document.querySelector('a[name^="freezeframe"]');
+  //     const mountNode = document.createElement('div');
+  //     anchor.parentElement.insertBefore(mountNode, anchor);
+  //     render(<TeamError />, mountNode);
+  //   }
+  //   // Either way, stop
+  //   return;
+  // }
 
   const scrollytellers = loadFreezeframes('u-full', 'mark');
   Object.keys(scrollytellers).forEach(key => {
